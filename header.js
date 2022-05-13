1c325bafd30dbe7656b0c3544af011664ed22ac0
ibnuzaman <ibnuzaman2133@gmail.com>
2022-05-14 00:34:36 +0700

    first commit

---
+++ header.js
@@ -0,0 +1,7 @@
+document.writeln("<CENTER>");
+document.writeln("<HR WIDTH=600 COLOR=Black>");
+document.writeln("<H1>UNIVERSITAS AHMAD DAHLAN</H1>");
+document.writeln("<H2>FAKULTAS TEKNOLOGI INDUSTRI<H2>");
+document.writeln("<H3>JURUSAN TEKNIK INFORMATIKA<H3>");
+document.writeln("<HR WIDTH=600 COLOR=Black>");
+document.writeln("</CENTER>");
