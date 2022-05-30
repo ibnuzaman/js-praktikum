deff76996e014ba1a82984fae3720c8ab3ba74b3
ibnuzaman <ibnuzaman2133@gmail.com>
2022-05-30 15:50:37 +0700

    update

---
+++ Pertemuan 7/postes/postest.js
@@ -0,0 +1,13 @@
+function lorem() {
+    var nama = (document.fform.inama.value);
+    var nim = (document.fform.inim.value);
+    var fak = (document.fform.ifak.value);
+    var prodi = (document.fform.iprodi.value);
+    var ang = (document.fform.iang.value);
+
+    document.fform.onama.value = nama;
+    document.fform.onim.value = nim;
+    document.fform.ofak.value = fak;
+    document.fform.oprodi.value = prodi;
+    document.fform.oang.value = ang;
+}
